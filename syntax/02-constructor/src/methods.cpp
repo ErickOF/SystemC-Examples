01b1c37f16a9f43df0c6cd9bba36d3cd5bbe4762
Erick Obregón Fonseca <erickobregonf@gmail.com>
2024-05-16 19:42:17 -0600

    [Update] Adding documentation for constructor syntax

--- syntax/02-constructor/src/methods.cpp
+++ syntax/02-constructor/src/methods.cpp
@@ -3,19 +3,32 @@
 using namespace sc_core;
 
 
+/**
+ * @brief Class for Module1
+ */
 SC_MODULE(Module1)
 {
+    /**
+     * @brief Constructor takes by default only the name of the module
+     */
     SC_CTOR(Module1)
     {
+        // SC_METHOD registers a new method to SystemC simulation kernel
         SC_METHOD(method1);
     }
 
+    /**
+     * @brief Method has no parameters (inputs) and returns (outputs)
+     */
     void method1()
     {
         std::cout << name() << std::endl;
     }
 };
 
+/**
+ * @brief Class for Module2
+ */
 SC_MODULE(Module2)
 {
     SC_CTOR(Module2)
@@ -23,20 +36,36 @@ SC_MODULE(Module2)
         SC_METHOD(method2);
     }
 
+    /**
+     * @brief Method is only declared
+     * 
+     */
     void method2();
 };
 
-
+/**
+ * @brief Method is defined outside of the class definition
+ * 
+ */
 void Module2::method2()
 {
     std::cout << this->name() << std::endl;
 }
 
+/**
+ * @brief Class for Module3
+ */
 SC_MODULE(Module3)
 {
     const int i;
 
     SC_CTOR(Module3);
+    /**
+     * @brief Construct takes more arguments
+     * 
+     * @param name - default module name
+     * @param i - additional argument
+     */
     Module3(sc_module_name name, int i) : sc_module(name), i(i)
     {
         SC_METHOD(method3);
